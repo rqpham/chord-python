fe85a94280c60a1c7bce77de67345360d57ddaa1
Remy Pham <rqpham@ucsc.edu>
2017-12-09 13:10:06 -0800

    framework compiles, need to get join working

--- chord.py
+++ chord.py
@@ -5,27 +5,34 @@ class FingerTable:
 		self.nodeID = nodeID
 		self.table = [None,None,None,None,None,None,None,None,None]
 		self.nodeTable = [None,None,None,None,None,None,None,None,None]
-		if nodeCount == 0:
+		if nodeCount == 1:
 			for x in range (1,9):
-				self.table[x] = nodeID              #this makes it an array of *nodeIDs*
-				self.nodeTable[x] = self            #this makes it an array of nodes
+				self.table[x] = nodeID              #this makes the F.T. an array of the key n+2^i
+				self.nodeTable[x] = self            #this makes the F.T. an array of nodes (just itself currently)
 		else:
 			for x in range(1,9):
-				self.table[x] = nodeID+(2**(x-1))   #this makes it an array of *nodeIDs*
-				self.nodeTable[x] = self            #this makes it an array of nodes
+				self.table[x] = nodeID+(2**(x-1))   #this makes the F.T. an array of the key n+2^i
+				self.nodeTable[x] = self            #this makes the F.T. an array of nodes (just itself currently)
 
 class Node:
 
 	def __init__(self, nodeID):
 		global nodeCount
 		self.nodeID = nodeID
-		self.table=FingerTable(nodeID)
-		self.successor=self.table.table[1]
-		self.predecessor=self.table.table[1]	
-		self.successorNode = self.table.nodeTable[1]	
-		self.predecessorNode = self.table.nodeTable[1]
 		nodeList[nodeCount]=self
 		nodeCount=nodeCount+1
+		self.table=FingerTable(nodeID)
+		if nodeCount==1:     #if it's the first node in the network, set its successor and predecessor to itself
+			self.successor=self.table.table[1]
+			self.predecessor=self.table.table[1]	
+			self.successorNode = self.table.nodeTable[1]	
+			self.predecessorNode = self.table.nodeTable[1]
+		else:
+			self.successor=self.table.table[1]
+			self.predecessor=self.table.table[1]	
+			self.successorNode = self.table.nodeTable[1]	
+			self.predecessorNode = self.table.nodeTable[1]			
+
 		#join(self) 
 
 	def printFingerTable(self):
@@ -47,14 +54,10 @@ def printNodeCount():
 def join(node):
 
 	if nodeCount == 1:
-
 		print "|This is the first node in the network.                       |"
-
 		#pass
-	else:
-		#join by giving it a random node in the topology. for beginning implementation we will continue to use the first node.		
+	else:                               #join by giving it a random node in the topology. for beginning implementation we will continue to use the first node.		
 		#implement join here
-
 		pass
 	
 	printAllFingerTables()
@@ -63,10 +66,7 @@ def join(node):
 def printAllFingerTables():
 	for x in range(0,nodeCount):
 		nodeList[x].printFingerTable()
-		
-
 		#need to implement correct print function, pulling from nodeList[node]
-
 
 #this is where the finger table is rebuilt after a node joins the ring
 def rebuildFingerTables():
